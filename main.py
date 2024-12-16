d02de9f84d93cb1e586cd9a6ed5f411262274566
Felipe Silva <felipe.estagiario@santiskin.com>
2024-12-16 17:59:44 +0000

    Changed navbar layout, created title and text on page

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from flask import Flask, render_template, url_for, flash, request
-
+#import openxls
+#os
 
 app = Flask(__name__)
 app.secret_key = 'LandingPage' #necessary for using flash messages
 
 @app.route('/')
 def home():
+    #create request to retrive info from form
+    #save info in an excel file
+    #use os module to make sure xls file does not exist
+
     return render_template('index.html')
 
 if __name__ == '__main__':
